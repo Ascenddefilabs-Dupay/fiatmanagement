3b99cb2f19b2dd94568585e440d24b269db91656
VikramMoger1 <Vikram.Moger@ascenddefi.com>
2024-08-12 14:06:28 +0530

    created fiat wallet

--- backend_fiatmanagement/currency_converter/serializers.py
+++ backend_fiatmanagement/currency_converter/serializers.py
@@ -1,8 +1,25 @@
 from rest_framework import serializers
 from .models import Project
-
+from .models import FiatWallet,User
 
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = Project
+        fields = '__all__'
+
+
+
+
+
+
+
+
+class FiatWalletSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FiatWallet
+        fields = '__all__'
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
         fields = '__all__'
--- backend_fiatmanagement/currency_converter/views.py
+++ backend_fiatmanagement/currency_converter/views.py
@@ -2,9 +2,21 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Project
 from .serializers import ProjectSerializer
+from .serializers import FiatWalletSerializer,UserSerializer
+from .models import FiatWallet
+from .models import User
 # Create your views here.
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+
+class FiatWalletViewSet(viewsets.ModelViewSet):
+    queryset = FiatWallet.objects.all()
+    serializer_class = FiatWalletSerializer
+    lookup_field="fiat_wallet_id"
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
