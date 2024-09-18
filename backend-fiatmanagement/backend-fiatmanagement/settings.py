8a552d0a01e1e35713bda23c58f034db4999db57
shivaprasadGow <Shivaprasad.G@ascenddefi.com>
2024-09-18 11:45:25 +0530

    create wallet

--- backend-fiatmanagement/backend-fiatmanagement/settings.py
+++ backend-fiatmanagement/backend-fiatmanagement/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'corsheaders',
-    'currency_converter',
+    'Topup',
     'cloudinary_storage',
     'cloudinary',
 ]
