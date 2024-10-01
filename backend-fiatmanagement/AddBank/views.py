e45036519ccf3c2c760b31a592f37fe56891b4f1
tej-2206 <sasi.venkat@ascenddefi.com>
2024-10-01 11:03:09 +0530

    addbank

--- backend-fiatmanagement/AddBank/views.py
+++ backend-fiatmanagement/AddBank/views.py
@@ -2,11 +2,21 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from django.http import JsonResponse
 from .models import Bank
+from django.shortcuts import get_object_or_404
 from .serializers import BankSerializer
 from django.db import connection
 import json
 
+@api_view(['GET'])
+def get_banks_by_user(request, user_id):
+    try:
+        banks = Bank.objects.filter(user_id=user_id)
+        serializer = BankSerializer(banks, many=True)
+        return Response(serializer.data, status=200)
+    except Exception as e:
+        return Response({'error': str(e)}, status=400)
 @api_view(['POST'])
 def add_bank(request):
     # Print request data for debugging
@@ -54,3 +64,18 @@ def add_bank(request):
         return Response({'message': 'Bank added successfully!'}, status=status.HTTP_201_CREATED)
     print("Serializer errors:", serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+def get_bank_details(request, user_id, bank_name):
+    try:
+        bank = get_object_or_404(Bank, user_id=user_id, bank_name=bank_name)
+        bank_data = {
+            'user_id': bank.user_id,
+            'account_holder_name': bank.account_holder_name,
+            'account_number': bank.account_number,
+            'ifsc_code': bank.ifsc_code,
+            'branch_name': bank.branch_name,
+            'bic_code': bank.bic_code,
+            'currency': bank.currency
+        }
+        return JsonResponse(bank_data)
+    except Bank.DoesNotExist:
+        return JsonResponse({'error': 'Bank not found'}, status=404)
