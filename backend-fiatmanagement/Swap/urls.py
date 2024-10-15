c2a1bf890696be93aa6c2ceca67c6796412c424a
VikramMoger1 <Vikram.Moger@ascenddefi.com>
2024-10-15 18:19:41 +0530

    updated fiat swap

--- backend-fiatmanagement/Swap/urls.py
+++ backend-fiatmanagement/Swap/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import get_currency_icon,get_all_currency_icons,get_fiat_wallet_by_user_id
+from .views import get_currency_icon,get_all_currency_icons,get_fiat_wallet_by_user_id,convert_currency
 
 urlpatterns = [
     path('get-currency-icon/', get_currency_icon, name='get-currency-icon'),
     path('icons/', get_all_currency_icons, name='get_all_currency_icons'),
     path('fiat_wallet/<str:wallet_id>/', get_fiat_wallet_by_user_id, name='get_fiat_wallet_by_user_id'),
+    path('convert_currency/', convert_currency, name='convert_currency'),
 ]
